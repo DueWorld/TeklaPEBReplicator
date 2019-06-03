3a6bf0bf2e9ea0b5fb0bc9a4ed7f910388f58f85
Mohamed El-Sayed <mohamedesayedmohsen@gmail.com>
2019-06-03 14:07:42 +0200

    Initialize

---
+++ PEBReplicator/PEBReplicator/TeklaForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using T3D = Tekla.Structures.Geometry3d;
+using TUI = Tekla.Structures.Model.UI;
+using Tekla.Structures;
+using Tekla.Structures.Model;
+
+namespace PEBReplicator
+{
+    public partial class TeklaForm : Form
+    {
+        public TeklaForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
